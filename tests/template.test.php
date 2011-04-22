61023d04a4500e7a08b8e4459cf5ee032b530c85
ubuntu <ubuntu@ubuntu-comp>
2011-04-22 17:50:28 +0400

    Добавлен в разработку модуль Template

--- tests/template.test.php
+++ tests/template.test.php
@@ -9,55 +9,57 @@
 $template_delete[] = array(
         'args'=>array('exist'),
         'etalon'=>true,
-        'description'=>'Проверка работоспособности при существующем шаблоне'
+        'description'=>'Проверка работоспособности удаления при существующем шаблоне'
 );
 
 $template_delete[] = array(
         'args'=>array(''),
         'etalon'=>false,
-        'description'=>'Проверка работоспособности при передаче пустой строки'
+        'description'=>'Проверка работоспособности удаления при передаче пустой строки'
 );
 
 $template_delete[] = array(
         'args'=>array('not_exist'),
         'etalon'=>false,
-        'description'=>'Проверка работоспособности при не существующем шаблоне'
+        'description'=>'Проверка работоспособности удаления при не существующем шаблоне'
 );
 
 
 
 $template_update[] = array(
-        'args'=>array('exist'),
+        'args'=>array('exist', array(1)),
         'etalon'=>true,
         'description'=>'Проверка работоспособности изменений при существующем шаблоне'
 );
 
 $template_update[] = array(
-        'args'=>array(''),
+        'args'=>array('', ''),
         'etalon'=>false,
         'description'=>'Проверка работоспособности изменений при передаче пустой строки'
 );
 
 $template_update[] = array(
-        'args'=>array('not_exist'),
+        'args'=>array('not_exist', array(1)),
         'etalon'=>false,
         'description'=>'Проверка работоспособности изменений при не существующем шаблоне'
 );
 
 
 $template_add[] = array(
-        'args'=>array('creating'),
+        'args'=>array('creating', ''),
         'etalon'=>true,
         'description'=>'Проверка работоспособности добавления шаблона'
 );
 
 $template_add[] = array(
-        'args'=>array('not_creating'),
+        'args'=>array('not_creating', ''),
         'etalon'=>false,
         'description'=>'Проверка работоспособности добавления шаблона'
 );
 
 
+utest_test_function('template_add', $template_add);
+utest_test_function('template_update', $template_update);
 utest_test_function('template_delete', $template_delete);
 //utest_message('Functions template_delete');
 ?>
