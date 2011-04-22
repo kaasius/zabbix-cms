d8560482b47ea4b0d19ef86fac5b17ed7958556c
ubuntu <ubuntu@ubuntu-comp>
2011-04-22 17:50:33 +0400

    Добавлен в разработку модуль Template

--- inc/template.inc.php
+++ inc/template.inc.php
@@ -3,6 +3,8 @@
 Библиотека для работы с шаблонами
  */
 
+
+//это для теста
 $tmp = '<div class="title"><a href="/archiv_news/group:{GROUPID}" class="groupnews">
         <img src="/design/title_news.gif" alt="" width="86" height="20" border="0"></a>
         </div>
@@ -12,23 +14,35 @@ $tmp = '<div class="title"><a href="/archiv_news/group:{GROUPID}" class="groupne
 
 
 template_add($tmp);
+//это для теста
+
+
 
 /*
  * Проверяем существует ли шаблон
  */
-function template_check($name) {
+function __template_check($name){
 
-	if ( is_file("./".$_SERVER['PHP_SELF']) )
+	if ( is_file($_SERVER['DOCUMENT_ROOT']."/".$name) )
 	    return true;
 	else
 	    return false;
+}
+
+/*
+ * Обновляем переменные
+ */
+function __template_update_vars($array_new_vars){
+    
+    $array_old_vars = array();
+    $array_vars = array_merge($array_old_vars, $array_new_vars);    
 }
 
 
 /*
  * Парсим переменные
  */
-function template_parse_vars($template) {
+function __template_parse_vars($template){
 
     $array_vars = array();
 
@@ -36,38 +50,38 @@ function template_parse_vars($template) {
 //    	echo "Список переменных: <br /><br />";
 	    for ($i=0; $i< count($vars[0]); $i++) {
 //		 echo "$i:  " . $vars[0][$i] . "<br />";
-		 $array_vars[$vars[0][$i]] = "";
+		 $array_vars[$vars[0][$i]] = array("value"=>"", "type"=>"text");
 		 }
 return $array_vars;
 }
 
 
 /*
+ * Создание файла (функция драйвера)
+ */
+function __template_create_file($path,$file){
+echo $path;
+    $f = file_put_contents($path, $file);
+        if (!is_file($path))
+            return false;        
+}
+
+
+/*
  * Добавление шаблона
  */
-function template_add($template){
-	if (template_check($template) == false)
+function template_add($template,$name="exist.htm"){
+	if (__template_check($template) == true || empty($template))
 		return false;
-	$array_vars = template_parse_vars($template);
+	$array_vars = __template_parse_vars($template);
+     
+$path = $_SERVER['DOCUMENT_ROOT']."/".$name;
 
-	if (empty($array_vars)) {
-	/*
-	Если в шаблонет переменных нет...
-	*/
-	    	return false;
-	    	}
 
-$name = $_SERVER['DOCUMENT_ROOT']."/tmpname.htm";
-$path = "".$name;
-$file = fopen($path, 'ab');
-if ($file == false)
-	return false;
-
-fwrite($file, $template);
-fclose($fp);
-echo "done";
-
-return true;
+if (__template_create_file($path,$template) == false)
+        return false;
+    else
+        return true;
 
 }
 
@@ -77,27 +91,37 @@ return true;
  */
 function template_delete($name){
 
-	if (template_check($template) == false)
-		return false;
+	if (__template_check($name) == false || empty($name))
+            return false;
 
+        if (unlink($_SERVER['DOCUMENT_ROOT']."/".$name) == false)
+            return false;
 
+    return true;        
+    
 }
 
 
 /*
  * Обновление шаблона
  */
-function template_update($name){
+function template_update($name,$array_new_vars){
 
-	if (template_check($template) == false)
-		return false;
-
-	if (is_file($name)){
-		template_add($name);
-		return true;
-		}
-
-
+	if (!is_file($name) && __template_check($name) == true){  
+            __template_update_vars($array_new_vars);
+            return true; 
+        } else if (is_file($name) && __template_check($name) == true) {
+            $array_new_vars = __template_parse_vars($name);  
+            __template_update_vars($array_new_vars);
+            return true;            
+        } else if (is_file($name) && __template_check($name) == false) {
+            template_add($name);
+            return true;           
+        } else {
+            return false;            
+        }
+        
+        
 
 }
 
