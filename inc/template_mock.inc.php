dd28474105f163963e03168518cec618f439e3c9
ubuntu <ubuntu@ubuntu-comp>
2011-04-27 13:02:48 +0400

    Заглушка для драйвера шаблонов

--- inc/template_mock.inc.php
+++ inc/template_mock.inc.php
@@ -5,7 +5,7 @@
  */
 
 
-save_template();
+load_template('not_exist');
 
 /*
  * Загружаем шаблон
@@ -15,11 +15,11 @@ save_template();
 function load_template(){
 
     $preset=array();
-    $preset['40cd750bba9870f18aada2478b24840a']=array( //существует шаблон -> загружаем
-        'template_name'=>'name',        
+    $preset['44006c0f5d1eaa95c6db0789266db2b0']=array( //существует шаблон -> загружаем
+        'template_name'=>'exist',        
         'vars'=>serialize(array(
-            'name_var'=>'',
-            'var_value'=>'',
+            'name_var'=>'GROUPID',
+            'var_value'=>'номер_группы',
             'type'=>'text'
             )),
         'html'=>'<div class="title"><a href="/archiv_news/group:{GROUPID}" class="groupnews">
@@ -30,7 +30,7 @@ function load_template(){
         <div align="right"><a href="/archiv_news/group:{GROUPID}" class="link_all_news">Все {NAME} &raquo;</a>'
         );
     
-    $preset['40cd750bba9870f18aada2478b24840a']=array( //шаблон не существует
+    $preset['cac3a07c1243196f5e369364183afcb2']=array( //шаблон не существует
         'template_name'=>'not_exist',
         'html'=>'',
         'vars'=>''
