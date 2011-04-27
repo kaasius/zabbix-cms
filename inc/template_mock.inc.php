13b789938bca2605a7e6c2437c5b0d88b7defa95
ubuntu <ubuntu@ubuntu-comp>
2011-04-27 13:19:56 +0400

    Заглушка для драйвера шаблонов

--- inc/template_mock.inc.php
+++ inc/template_mock.inc.php
@@ -5,17 +5,16 @@
  */
 
 
-load_template('not_exist');
 
 /*
  * Загружаем шаблон
  */
 
 
-function load_template(){
+function load_template($name, $vars, $html){
 
     $preset=array();
-    $preset['44006c0f5d1eaa95c6db0789266db2b0']=array( //существует шаблон -> загружаем
+    $preset['1251ffef7a077b76711eb4bfa838656e']=array( //существует шаблон -> загружаем
         'template_name'=>'exist',        
         'vars'=>serialize(array(
             'name_var'=>'GROUPID',
@@ -30,7 +29,7 @@ function load_template(){
         <div align="right"><a href="/archiv_news/group:{GROUPID}" class="link_all_news">Все {NAME} &raquo;</a>'
         );
     
-    $preset['cac3a07c1243196f5e369364183afcb2']='false'; //шаблон не существует    
+    $preset['0d41994c811fb3f9f0155e05ffa74917']=false; //шаблон не существует    
 
 
     $key=md5(serialize(func_get_args()));
@@ -46,10 +45,10 @@ function load_template(){
 }
 
 
-function save_template(){
+function save_template($name, $html, $create){
 
     $preset=array();
-    $preset['4287c37180b5760567c04ae3f654de3a']=array( //шаблона не существует -> создаём
+    $preset['56d1c23025e6b4d9de432a13ee2fa663']=array( //шаблона не существует -> создаём
         'template_name'=>'not_exist',
         'html'=>'<div class="title"><a href="/archiv_news/group:{GROUPID}" class="groupnews">
         <img src="/design/title_news.gif" alt="" width="86" height="20" border="0"></a>
@@ -61,10 +60,10 @@ function save_template(){
         );
     
     $preset=array();
-    $preset['0c7acb9ab4214df7a55c5223bfb8625c']='false'; //шаблон существует и перезапись запрещена
+    $preset['0d41994c811fb3f9f0155e05ffa74917']=false; //шаблон существует и перезапись запрещена
 
     $preset=array();
-    $preset['0422ac27529862c48bca986f48021129']=array( //шаблон сущесвует и перезапись разрешена
+    $preset['b24d8c5b23bd7e21e00d83bab6cbcf03']=array( //шаблон сущесвует и перезапись разрешена
         'template_name'=>'exist',
         'html'=>'<div class="title"><a href="/archiv_news/group:{GROUPID}" class="groupnews">
         <img src="/design/title_news.gif" alt="" width="86" height="20" border="0"></a>
