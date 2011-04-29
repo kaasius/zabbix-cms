2ae1b2251848c50d8f7e4de30ae0868c9acea120
ubuntu <ubuntu@ubuntu-comp>
2011-04-29 16:37:57 +0400

    Создание языковых файлов

---
+++ lang/ru/lang_error.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang_error["error_key"] = "В именах переменных запрещено использовать: null, yes, no, true, false, on, off, none";
+$lang_error["error"] = "Ошибка: ";
+$lang_error["error_w"] = "Ошибка в";
+$lang_error["empty_name"] = "Не задано имя";
+$lang_error["empty_tpl"] = "Пустой файл";
+$lang_error["tmp_file"] = "Не удалось записать файл шаблона";
+$lang_error["ini_file"] = "Не удалось записать файл переменных";
+$lang_error["save_template"] = "Шаблон существует, перезапись не разрешена";
+$lang_error["tmp_not_exist"] = "Файл шаблона не существует. Внезапно...";
+$lang_error["lib_tmp"] = "Библиотека шаблонов";
+$lang_error["driver_tmp"] = "Драйвер шаблонов";
+$lang_error["del"] = "Не удалось удалить шаблон: либо он не существует, либо не достаточно прав.";
+$lang_error["error_upd"] = "Обновление шаблона работает не корректно";
+?>
