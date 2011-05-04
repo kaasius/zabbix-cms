7de1c8d23e116a6c1a372e8d8e4eac483a25b49a
ubuntu <ubuntu@ubuntu-comp>
2011-05-04 14:35:58 +0400

    Удаление драйвера пользователей и перенос его функций в библиотеку. Обновление библиотеки пользователей (v.0.2)

--- inc/users.inc.php
+++ inc/users.inc.php
@@ -4,76 +4,83 @@
  * Библиотека для работы с пользователями
  */
 define(SALT, 'zabbix-cms');
+if(!defined('TEST_MODE')) define("TEST_MODE",false);
+define('DEBUG_MODE',true);
+require_once("errors.list.php");
+require ("$HTTP_SERVER_VARS[DOCUMENT_ROOT]/config.inc.php");
+require ("$inc_path/_db.inc.php");
 
-function users_add($email,$pass){
+
+
+function users_valid_email($email){
+    if (!preg_match('/^([0-9a-zA-Z]([-.w]*[0-9a-zA-Z])*@([0-9a-zA-Z]*.)+[a-zA-Z]{2,9})$/si', $email)){
+        return false;
+    } else 
+        return true;
+}
+
+function users_add_post($email,$pass){
+    $email = $_POST["email"];
+    $password = $_POST["password"];    
     if (empty($email)){
         return false;
     }
-    if (!preg_match('/^([0-9a-zA-Z]([-.w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-w]*[0-9a-zA-Z].)+[a-zA-Z]{2,9})$/si', $email)){
+    if (users_valid_email($email) == false){
         return false;
     }
     if (empty($pass)){
         return false;
     } 
     $password = md5($pass.SALT);
-    if (driver_add_user($email, $password) == false)
+    if (users_add_user($email, $password) == false){
             return false;
+    }
     return true;
 }
 
 function users_delete($id){
-    if (driver_del($id) == false)
+    if (users_del($id) == false)
             return false;
     return true;    
 }
 
 function users_update($id,$array){
-    $user_array = driver_load_user($id);
+    $user_array = users_load_user($id);
     if(empty($user_array)){
         return false;
     }
     return true;    
 }
 
-function users_auth($username,$pswd){
-    if (empty($pswd)){
+function users_auth(){
+    session_start();
+    if (empty($_SESSION['userid'])){
         return false;
-    }
-    if (empty($username)){
-        return false;
-    }    
-    $user_array = driver_load_user($username);
-    if ($user_array == false) {
-        return false;
-    }   
-    $password = md5($pswd.SALT);
-    $real_password = $user_array['password'];
-    $real_username = $user_array['email'];    
-    if ($password == $real_password && $username == $real_username){
-        return true;
     } else {
-        return false;
+        return true;
     }
 }
 
-function users_login($email,$password){
+function users_login_post(){
+    $email = $_POST["email"];
+    $password = $_POST["password"];     
     if (empty($email)){
         return false;
     }
     if (empty($password)){
         return false;
     } 
-    $user_array = driver_load_user($email);
+    $user_array = users_load_user($email);
     if ($user_array == false) {
         return false;
     }
     $password = md5($password.SALT);
     $real_password = $user_array['password'];
     $real_email = $user_array['email'];
-    if ($password == $real_password && $email == $real_email){
+    if ($password == $real_password){
         session_start();
-        $_SESSION['username'] == $email;
-        $_SESSION['pwd'] == $password;
+        $_SESSION['userid'] = $user_array['id'];
+//        $_SESSION['username'] = $user_array['email'];
         return true;
     } else {
         return false;
@@ -81,29 +88,46 @@ function users_login($email,$password){
 }
 
 
-function users_logout($email){
-    if (empty($email)){
-        return false;
-    }
-//    if (empty($pass)){
-//        return false;
-//    } 
-    $user_array = driver_load_user($email);
-    if ($user_array == false) {
-        return false;
-    }    
+function users_logout_post($email){
     session_start();
-    $password = md5($_SESSION['pwd'].SALT);
-    $real_password = $user_array['password'];
-    $real_email = $user_array['email'];
-    if ($password == $real_password && $_SESSION['username'] == $real_email){
-        $_SESSION['username'] == '';
-        $_SESSION['pwd'] == '';
+    if (empty($_SESSION['userid'])){
+        return false;
+    } else {
+        unset($_SESSION['userid']);
+//        unset($_SESSION['username']);
         session_destroy();
         return true;
-    } 
-    else {
+    }
+}
+
+
+
+
+function users_add_user($email, $password){
+    echo $email;
+    $query = ("INSERT INTO users (email, password) VALUES(?, ?)");
+    if (query($query, $email, $password) == false){
+        echo "-";
         return false;
-    }       
+    } else {
+    echo "+";
+    return true;
+    }
+}
+
+function users_load_user($email){
+    $query = selectrow("SELECT * from users WHERE email = ?", $email);
+    if ($query == false){
+        return false;
+    }
+    return $query;
+}
+
+function users_del($id){
+    $query = ("DELETE from users WHERE id=?");
+    if (query($query, $id) == false){ 
+        return false;
+    }
+    return true;       
 }
 ?>
